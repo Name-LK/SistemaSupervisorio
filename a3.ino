db81c29a4b9beeeb6f2d0f2f7228b2b82e4e3294
Name-LK <l250799k@outlook.com>
2024-04-12 00:10:37 -0300

    ARQUIVOS ATUALIZADOS DIA 2024-04-12

--- a3.ino
+++ a3.ino
@@ -15,11 +15,15 @@ int umidade;
 #define DHTTYPE DHT11
 
 #define ldrPin A2
+
 #define led0 2
 #define led1 3
 #define led2 6
 #define led3 7
-#define led4 8
+
+
+#define ist 8
+#define botaoPin 9
 
 // Inicializa o sensor nos pino 4 e 5 
 Ultrasonic ultrasonic(triggerPin, echoPin);
@@ -32,7 +36,7 @@ void setup() {
     // The media access control (ethernet hardware) address for the shield
     byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  
     // The IP address for the shield
-    byte ip[] = { 192, 168, 0, 120 };   
+    byte ip[] = { 192, 168, 1, 120 };   
     //Config Modbus IP 
     mb.config(mac, ip);
     dht.begin(); // inicializa o sensor DHT11
@@ -52,7 +56,9 @@ void setup() {
     mb.addCoil(1);
     mb.addCoil(2);
     mb.addCoil(3);
-    mb.addCoil(4);
+    
+
+    mb.addIsts(0);
 
 }
 
@@ -67,23 +73,19 @@ void loop() {
    int luminosidade = analogRead(ldrPin);                         //IREG
    int distancia = ultrasonic.convert(microsec, Ultrasonic::CM); //IREG
    umidade  = analogRead(umidadePin);                           //IREG
-   umidade = map(umidade, 1023, 315, 0, 100);
-   Serial.print("umidade do solo  " );
-   Serial.print(umidade);
-   Serial.print(" %");
-   Serial.println("");
+
    mb.Ireg(0, umidadeAr);
    mb.Ireg(1, temperaturaAr);
    mb.Ireg(2, luminosidade);
    mb.Ireg(3, distancia);
    mb.Ireg(4, umidade);
 
+   mb.Ists(0, digitalRead(botaoPin));
+
    digitalWrite(led0, mb.Coil(0));
    digitalWrite(led1, mb.Coil(1));
    digitalWrite(led2, mb.Coil(2));
    digitalWrite(led3, mb.Coil(3));
-   digitalWrite(led4, mb.Coil(4));
-
-   Serial.println(umidade);
+   
 
 }
